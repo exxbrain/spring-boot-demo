cb34202f08d5ed317278ca51edf573126fd5a58f
Denis Zakharov <exxbrain@gmail.com>
2020-02-15 22:50:28 +0300

    Add delete all functionality

---
+++ src/main/java/com/exxbrain/springbootdemo/web/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.exxbrain.springbootdemo.web;
+
+import com.exxbrain.springbootdemo.domain.repository.EmployeeRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RepositoryRestController
+public class EmployeeController {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @DeleteMapping("/employees")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteAll() {
+        employeeRepository.deleteAll();
+    }
+}
